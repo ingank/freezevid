a49b536d6baf902d9c6769db572a074dc5267302
Ingolf Ankert <github@ingank.net>
2022-06-19 13:41:36 +0200

    Update generate_img.bat

--- generate_img.bat
+++ generate_img.bat
@@ -4,11 +4,11 @@ magick convert template_img.bmp ^
 -font Bahnschrift ^
 -pointsize 60 ^
 -draw "gravity NorthWest fill white text 30,100 ''" ^
--draw "gravity NorthWest fill white text 30,155 'Minecraft OST'" ^
--draw "gravity NorthWest fill white text 30,240 'Pigstep'" ^
+-draw "gravity NorthWest fill white text 30,155 'Interpret 1'" ^
+-draw "gravity NorthWest fill white text 30,240 'Titel 1'" ^
 -draw "gravity NorthWest fill white text 30,295 ''" ^
 -draw "gravity NorthEast fill white text 30,100 ''" ^
--draw "gravity NorthEast fill white text 30,155 'Exiter'" ^
--draw "gravity NorthEast fill white text 30,240 'Dream Running'" ^
--draw "gravity NorthEast fill white text 30,295 'Music'" ^
+-draw "gravity NorthEast fill white text 30,155 'Interpret 2'" ^
+-draw "gravity NorthEast fill white text 30,240 'Titel 2'" ^
+-draw "gravity NorthEast fill white text 30,295 ''" ^
 png.png
